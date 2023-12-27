da3ea79bba6441db1ccdf9c8ad057e4edf4544a5
codekmj1 <codekmj@gmail.com>
2023-12-27 23:01:46 +0900

    Add another Controller and Service and Exception

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ repositories {
 }
 
 dependencies {
+    implementation("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
---
+++ src/main/kotlin/com/teamsparta/courseregistration/domain/user/service/UserService.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.courseregistration.domain.user.service
+
+import com.teamsparta.courseregistration.domain.user.dto.SignUpRequest
+import com.teamsparta.courseregistration.domain.user.dto.UpdateUserProfileRequest
+import com.teamsparta.courseregistration.domain.user.dto.UserResponse
+
+interface UserService {
+
+    fun signUp(signUpRequest: SignUpRequest): UserResponse
+
+    fun updateUserProfile(userId: Long, updateUserProfileRequest: UpdateUserProfileRequest): UserResponse
+}
+
